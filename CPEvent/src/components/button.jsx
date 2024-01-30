d5d70cde8afce8e5b249dc03d27f9ff7e78504ad
BezTTTT <thanakit_chanlaset@cmu.ac.th>
2024-01-30 16:41:33 +0700

    add image to Button

--- CPEvent/src/components/button.jsx
+++ CPEvent/src/components/button.jsx
@@ -1,10 +1,18 @@
-export const Button = ({ label }) => (
-  <div className="flex items-center justify-between">
+export const Button = ({ label, imageSrc }) => (
+  <div className="flex items-center ">
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
       type="button"
     >
-      {label}
+      <div className="flex">
+        <div>
+          {imageSrc && (
+            <img src={imageSrc} style={{ width: "20px", height: "20px" }} />
+          )}
+        </div>
+        <div>{label}</div>
+      </div>
     </button>
   </div>
 );
+// add image to button
--- CPEvent/src/components/navBar.jsx
+++ CPEvent/src/components/navBar.jsx
@@ -1,45 +1,158 @@
-export function NavBar() {
+import React, { useState } from "react";
+import CPEvent from "../images/CPEvent.png";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-5">
-        <a class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8"
-            alt="Flowbite Logo"
-          />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            CPEvent
-          </span>
-        </a>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
+    <nav class="bg-baseblue border-gray-200 ">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* Logo */}
+        <Link
+          to="/login"
+          class="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img src={CPEvent} class="h-8" alt="Logo" />
+        </Link>
+
+        <div class="flex flex-wrap item-left ">
+          <div class="flex flex-wrap items-center justify-between md:order-1">
+            {/* Profile */}
+            <div class="flex  items-center md:order-2 rtl:space-x-reverse ml-5">
+              <button
+                type="button"
+                class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                onClick={toggleDropdown}
               >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="w-8 h-8 rounded-full"
+                  src="https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt="user photo"
+                />
+              </button>
+              {/* <!-- Dropdown menu --> */}
+              {isDropdownOpen && (
+                <div
+                  class=" z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow "
+                  id="user-dropdown"
+                >
+                  <div class="px-4 py-3">
+                    <span class="block text-sm text-gray-900 dark:text-white">
+                      Bonnie Green
+                    </span>
+                    <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                      name@flowbite.com
+                    </span>
+                  </div>
+                  <ul class="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Dashboard
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Settings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Earnings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Sign out
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              )}
+
+              {/* dropdown end */}
+              <button
+                data-collapse-toggle="navbar-user"
+                type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-user"
+                aria-expanded="false"
               >
-                My Group
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                <span class="sr-only">Open main menu</span>
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            {/* svg */}
+            <button class="hidden md:flex md:w-auto ml-5">
+              <svg
+                class="w-6 h-6 text-white "
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
               >
-                Bookmark
-              </a>
-            </li>
-          </ul>
+                <path d="M17.1 12.6v-1.8A5.4 5.4 0 0 0 13 5.6V3a1 1 0 0 0-2 0v2.4a5.4 5.4 0 0 0-4 5.5v1.8c0 2.4-1.9 3-1.9 4.2 0 .6 0 1.2.5 1.2h13c.5 0 .5-.6.5-1.2 0-1.2-1.9-1.8-1.9-4.2ZM8.8 19a3.5 3.5 0 0 0 6.4 0H8.8Z" />
+              </svg>
+            </button>
+          </div>
+
+          {/* tab */}
+          <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto"
+            id="navbar-user"
+          >
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-baseblue ">
+              <li>
+                <Link
+                  to="/component"
+                  class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-tranblue md:p-0 "
+                  aria-current="page"
+                >
+                  My group
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-tranblue md:p-0 "
+                >
+                  Bookmark
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
