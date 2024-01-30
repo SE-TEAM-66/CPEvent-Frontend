21de33732b6d1038fb9a2253fa60540d5894eac3
gusntc <Natacha_rungbanpant@cmu.ac.th>
2024-01-30 23:42:35 +0700

    "tailwind-config , images and component"

--- CPEvent/tailwind.config.js
+++ CPEvent/tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        baseblue: {
+          100: "#A7BCD9",
+          200: "#5D8FD8",
+          300: "#37628D",
+        },
+        basegreen: {
+          100: "#B2DB75",
+          200: "#546B34",
+        },
+        basegray: {
+          100: "#8E9186",
+        },
+      },
+      fontFamily: {
+        mitr: ["Mitr", "sans-serif"],
+        poppin: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
