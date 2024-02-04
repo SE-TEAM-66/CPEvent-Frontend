7104aafd91b400d583751933598ea40a6b329168
Nuttadon <nuttadon_p@cmu.ac.th>
2024-02-04 18:32:00 +0700

    Fix Searchbar

--- CPEvent/src/components/SearchBar.jsx
+++ CPEvent/src/components/SearchBar.jsx
@@ -2,7 +2,13 @@ import { Button } from "../components/button";
 
 export default function SearchBar() {
   return (
-    <form className="w-1/2">
+    <form className="w-full">
+      <label
+        for="default-search"
+        className="mb-2 text-sm font-poppins text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
       <div className="relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg
@@ -28,7 +34,12 @@ export default function SearchBar() {
           placeholder="Search Mockups, Logos..."
           required
         />
-        <Button label={"search"} />
+        <button
+          type="submit"
+          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Search
+        </button>
       </div>
     </form>
   );
