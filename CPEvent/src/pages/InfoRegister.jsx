3df0fefa41d0d772650cd37709bfac885197ac84
Nuttadon <nuttadon_p@cmu.ac.th>
2024-02-04 10:24:12 +0700

    fix InofRegister and Register

--- CPEvent/src/pages/InfoRegister.jsx
+++ CPEvent/src/pages/InfoRegister.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import Cookies from "js-cookie";
 
 export default function InfoRegister() {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    Fname: "",
+    Lname: "",
+    Email: "",
+    Password: "",
+    Password_conf: "",
+  });
+  const handleSignUp = async (e) => {
+    e.preventDefault(); //essential for button(guard band)
+    try {
+      if (userData.Password != userData.Password_conf) return;
+      await axios.post("http://localhost:4000/sign_up", userData, {
+        withCredentials: true,
+      });
+      navigate("/login");
+      // Handle the response as needed (e.g., show a success message)
+    } catch (error) {
+      // Handle errors (e.g., show an error message)
+      console.error("Signup Error:", error);
+    }
+  };
   return (
     <div>
       <div className="static">
@@ -18,7 +47,10 @@ export default function InfoRegister() {
       <div className="min-w-screen min-h-screen bg-baseblue-300 flex items-center justify-center px-5 py-5">
         <div className="bg-gray-100 text-basegray-200 rounded-xl shadow-xl max-w-7xl  overflow-hidden">
           <div className="md:flex w-full">
-            <div className="w-full py-10 px-5 md:px-10 font-poppin ">
+            <form
+              onSubmit={handleSignUp}
+              className="w-full py-10 px-5 md:px-10 font-poppin "
+            >
               <div className="text-left mb-10 ">
                 <h1 className=" text-3xl text-basegray-200 font-bold">
                   Register
@@ -26,7 +58,7 @@ export default function InfoRegister() {
                 <p>Enter your information to register</p>
               </div>
               <div>
-                <label for="" className="text-xs font-semibold px-1">
+                <label htmlFor="" className="text-xs font-semibold px-1">
                   Full name
                 </label>
                 {/* Firstname */}
@@ -37,6 +69,11 @@ export default function InfoRegister() {
                         <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
+                        required
+                        value={userData.Fname}
+                        onChange={(e) =>
+                          setUserData({ ...userData, Fname: e.target.value })
+                        }
                         type="text"
                         className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="First name"
@@ -50,6 +87,11 @@ export default function InfoRegister() {
                         <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
+                        required
+                        value={userData.Lname}
+                        onChange={(e) =>
+                          setUserData({ ...userData, Lname: e.target.value })
+                        }
                         type="text"
                         className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="Last name"
@@ -60,7 +102,7 @@ export default function InfoRegister() {
                 {/* Email */}
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-5">
-                    <label for="" className="text-xs font-semibold px-1">
+                    <label htmlFor="" className="text-xs font-semibold px-1">
                       Email
                     </label>
                     <div className="flex">
@@ -68,7 +110,12 @@ export default function InfoRegister() {
                         <i className="mdi mdi-email-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
+                        required
                         type="email"
+                        value={userData.Email}
+                        onChange={(e) =>
+                          setUserData({ ...userData, Email: e.target.value })
+                        }
                         className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="user@example.com"
                       />
@@ -78,7 +125,7 @@ export default function InfoRegister() {
                 {/* password */}
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-5">
-                    <label for="" className="text-xs font-semibold px-1">
+                    <label htmlFor="" className="text-xs font-semibold px-1">
                       Password
                     </label>
                     <div className="flex">
@@ -86,6 +133,11 @@ export default function InfoRegister() {
                         <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
+                        required
+                        value={userData.Password}
+                        onChange={(e) =>
+                          setUserData({ ...userData, Password: e.target.value })
+                        }
                         type="password"
                         className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="************"
@@ -96,7 +148,7 @@ export default function InfoRegister() {
                 {/* comfirm password */}
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-12">
-                    <label for="" className="text-xs font-semibold px-1">
+                    <label htmlFor="" className="text-xs font-semibold px-1">
                       Confirm Password
                     </label>
                     <div className="flex">
@@ -104,6 +156,14 @@ export default function InfoRegister() {
                         <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
+                        required
+                        value={userData.Password_conf}
+                        onChange={(e) =>
+                          setUserData({
+                            ...userData,
+                            Password_conf: e.target.value,
+                          })
+                        }
                         type="password"
                         className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="************"
@@ -113,13 +173,16 @@ export default function InfoRegister() {
                 </div>
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-5">
-                    <button className="block w-full max-w-xs mx-auto bg-baseblue-300 hover:bg-baseblue-100 text-white rounded-lg px-3 py-3 ">
+                    <button
+                      type="submit"
+                      className="block w-full max-w-xs mx-auto bg-baseblue-300 hover:bg-baseblue-100 text-white rounded-lg px-3 py-3 "
+                    >
                       REGISTER
                     </button>
                   </div>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
--- CPEvent/src/pages/Register.jsx
+++ CPEvent/src/pages/Register.jsx
@@ -1,8 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import InfoRegister from "./InfoRegister";
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import Cookies from "js-cookie";
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = async (e) => {
+    e.preventDefault(); //essential for button(guard band)
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/log_in",
+        {
+          Email: userData.username,
+          Password: userData.password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      Cookies.set("Authorization", response.date, { expires: 1 });
+      navigate("/page3");
+      // Handle the response as needed (e.g., show a success message)
+    } catch (error) {
+      // Handle errors (e.g., show an error message)
+      console.error("Login Error:", error);
+    }
+  };
+  const handleGetUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/get_users", {
+        withCredentials: true,
+      });
+      console.log(response.data);
+    } catch (error) {
+      // Handle errors (e.g., show an error message)
+      console.error("Get users fail:", error);
+    }
+  };
   return (
     <div>
       <div className="flex h-screen">
@@ -15,6 +58,7 @@ export default function Register() {
               LinkedIn for academic purposes
             </p>
             <button
+              onClick={handleGetUsers}
               type="button"
               className="text-white bg-baseblue-200 hover:bg-baseblue-100 font-poppin rounded-lg text-xs px-7 py-2 me-2 mb-2 "
             >
@@ -36,7 +80,7 @@ export default function Register() {
         <div className="px-4 md:px-0 w-1/3 flex items-center justify-center">
           {/* container */}
           <div className="max-w-md ">
-            <form>
+            <form onSubmit={handleLogin}>
               {/* greeting title */}
               <div>
                 <span className="mb-2 font-poppin font-bold text-2xl text-basegray-200">
@@ -50,44 +94,50 @@ export default function Register() {
               {/* <!--Username input--> */}
               <div className="relative mb-4" data-te-input-wrapper-init>
                 <input
+                  required
                   type="text"
-
+                  value={userData.username}
+                  onChange={(e) =>
+                    setUserData({ ...userData, username: e.target.value })
+                  }
                   className="peer block min-h-[auto] w-full rounded border-1 border-basegray-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   id="exampleFormControlInput1"
                   placeholder="Username"
                 />
                 <label
-                  for="exampleFormControlInput1"
+                  htmlFor="exampleFormControlInput1"
                   className="pointer-events-none absolute left-3 top-0 mb-0  font-poppin max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
                 >
                   Username
                 </label>
-
               </div>
 
               {/* <!--Password input--> */}
               <div className="relative mb-4" data-te-input-wrapper-init>
                 <input
+                  required
                   type="password"
-
+                  value={userData.password}
+                  onChange={(e) =>
+                    setUserData({ ...userData, password: e.target.value })
+                  }
                   className="peer block min-h-[auto] w-full rounded border-1 border-basegray-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   id="exampleFormControlInput11"
                   placeholder="Password"
                 />
                 <label
-                  for="exampleFormControlInput11"
+                  htmlFor="exampleFormControlInput11"
                   className="pointer-events-none absolute left-3 top-0 mb-0 font-poppin max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
                 >
                   Password
                 </label>
-
               </div>
 
               {/* <!--Submit button--> */}
               <div className="mb-2 pb-1 pt-1 text-center">
                 <button
                   className="mb-3 inline-block w-full bg-baseblue-300 rounded px-6 pb-2 pt-2.5 text-xs font-medium font-poppin uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                  type="button"
+                  type="submit"
                   data-te-ripple-init
                   data-te-ripple-color="light"
                 >
@@ -169,21 +219,19 @@ export default function Register() {
               </div>
 
               {/* <!--Register button--> */}
-
               <div className="flex items-center justify-between pb-6">
                 <p className="mb-0 mr-2 font-poppin text-sm">
                   Don't have an account?
                 </p>
-                <Link to="/infoRegister">
-                  <button
-                    type="button"
-                    className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 "
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    Register
-                  </button>
-                </Link>
+                <button
+                  type="button"
+                  onClick={() => navigate("/register")}
+                  className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Register
+                </button>
               </div>
             </form>
           </div>
