f31b52f41d0e50d716fde15786bcf71d750bfe05
Ixac <90751114+Ixac@users.noreply.github.com>
2024-03-04 19:58:25 +0700

    add profile / profile edit

--- CPEvent/src/pages/login.jsx
+++ CPEvent/src/pages/login.jsx
@@ -1,58 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
-import Cookies from "js-cookie";
+import React from "react";
+import { Link } from "react-router-dom";
+import Register from "./Register";
 
-export default function Register() {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-        document.body.style.overflow = "scroll"
-    };
-  }, []);
-  const navigate = useNavigate();
-  const [userData, setUserData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleLogin = async (e) => {
-    e.preventDefault(); //essential for button(guard band)
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/log_in",
-        {
-          Email: userData.username,
-          Password: userData.password,
-        },
-        {
-          withCredentials: true,
-        }
-      );
-      console.log(response.data)
-      Cookies.set("Authorization", response.data, { expires: 10 });
-      navigate("/");
-      // Handle the response as needed (e.g., show a success message)
-    } catch (error) {
-      // Handle errors (e.g., show an error message)
-      console.error("Login Error:", error);
-    }
-  };
-  const handleGetUsers = async () => {
-    try {
-      const response = await axios.get("http://localhost:4000/get_users", {
-        withCredentials: true,
-      });
-      console.log(response.data);
-    } catch (error) {
-      // Handle errors (e.g., show an error message)
-      console.error("Get users fail:", error);
-    }
-  };
+export default function Login() {
   return (
     <div>
       <div className="flex h-screen">
@@ -65,7 +15,6 @@ export default function Register() {
               LinkedIn for academic purposes
             </p>
             <button
-              onClick={handleGetUsers}
               type="button"
               className="text-white bg-baseblue-200 hover:bg-baseblue-100 font-poppin rounded-lg text-xs px-7 py-2 me-2 mb-2 "
             >
@@ -87,7 +36,7 @@ export default function Register() {
         <div className="px-4 md:px-0 w-1/3 flex items-center justify-center">
           {/* container */}
           <div className="max-w-md ">
-            <form onSubmit={handleLogin}>
+            <form>
               {/* greeting title */}
               <div>
                 <span className="mb-2 font-poppin font-bold text-2xl text-basegray-200">
@@ -101,38 +50,40 @@ export default function Register() {
               {/* <!--Username input--> */}
               <div className="relative mb-4" data-te-input-wrapper-init>
                 <input
-                  required
-                  type="email"
-                  value={userData.username}
-                  onChange={(e) =>
-                    setUserData({ ...userData, username: e.target.value })
-                  }
-                  className="peer block min-h-[auto] w-full rounded border-1 border-basegray-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none"
+                  type="text"
+                  className="peer block min-h-[auto] w-full rounded border-1 border-basegray-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   id="exampleFormControlInput1"
-                  placeholder="Email"
+                  placeholder="Username"
                 />
+                <label
+                  for="exampleFormControlInput1"
+                  className="pointer-events-none absolute left-3 top-0 mb-0  font-poppin max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                >
+                  Username
+                </label>
               </div>
 
               {/* <!--Password input--> */}
               <div className="relative mb-4" data-te-input-wrapper-init>
                 <input
-                  required
                   type="password"
-                  value={userData.password}
-                  onChange={(e) =>
-                    setUserData({ ...userData, password: e.target.value })
-                  }
-                  className="peer block min-h-[auto] w-full rounded border-1 border-basegray-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none"
+                  className="peer block min-h-[auto] w-full rounded border-1 border-basegray-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   id="exampleFormControlInput11"
                   placeholder="Password"
                 />
+                <label
+                  for="exampleFormControlInput11"
+                  className="pointer-events-none absolute left-3 top-0 mb-0 font-poppin max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                >
+                  Password
+                </label>
               </div>
 
               {/* <!--Submit button--> */}
               <div className="mb-2 pb-1 pt-1 text-center">
                 <button
                   className="mb-3 inline-block w-full bg-baseblue-300 rounded px-6 pb-2 pt-2.5 text-xs font-medium font-poppin uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                  type="submit"
+                  type="button"
                   data-te-ripple-init
                   data-te-ripple-color="light"
                 >
@@ -158,16 +109,16 @@ export default function Register() {
                   <svg
                     className="h-6 w-6 mr-2"
                     xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
                     viewBox="-0.5 0 48 48"
                     version="1.1"
                   >
                     <g
                       id="Icons"
                       stroke="none"
-                      strokeWidth="1"
+                      stroke-width="1"
                       fill="none"
-                      fillRule="evenodd"
+                      fill-rule="evenodd"
                     >
                       <g
                         id="Color-"
@@ -218,15 +169,16 @@ export default function Register() {
                 <p className="mb-0 mr-2 font-poppin text-sm">
                   Don't have an account?
                 </p>
-                <button
-                  type="button"
-                  onClick={() => navigate("/register")}
-                  className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  Register
-                </button>
+                <Link to="/Register">
+                  <button
+                    type="button"
+                    className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 "
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Register
+                  </button>
+                </Link>
               </div>
             </form>
           </div>
