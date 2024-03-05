5f135c0271c01ec3433e07bab1612d18a053c230
Natacha Rungbanpant <Natacha_rungbanpant@cmu.ac.th>
2024-03-05 21:13:02 +0700

    "Make Responsive page"

--- CPEvent/src/components/Mygroup.jsx
+++ CPEvent/src/components/Mygroup.jsx
@@ -3,10 +3,10 @@ import { Avatar } from "flowbite-react";
 
 export default function Mygroup() {
   return (
-    <div>
+    <div class="flex-none mx-2">
       <button class="m-2">
         <div class="flex space-x-3">
-          <div>
+          <div class="flex-shrink-0">
             {/* online:green busy:red offline:gray away:yellow */}
             <Avatar
               img="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
@@ -16,10 +16,12 @@ export default function Mygroup() {
               size="sm"
             />
           </div>
-          <div class="items-center font-poppin my-auto text-baseblue-300 text-sm">
+          <div class="items-center font-poppin my-auto text-baseblue-300 text-sm whitespace-nowrap">
             Group name
           </div>
-          <div class="font-poppin my-auto text-baseblue-300 text-sm">3/5</div>
+          <div class="font-poppin my-auto text-baseblue-300 text-sm whitespace-nowrap">
+            3/5
+          </div>
         </div>
       </button>
     </div>
