9dc9c8da8134924440eefb2c414cbe289d7512f7
Wuttipat Sanchai <wuttipat.gamxu@gmail.com>
2024-03-06 17:27:20 +0700

    implement join group btn

--- CPEvent/src/components/MemberRequire.jsx
+++ CPEvent/src/components/MemberRequire.jsx
@@ -54,7 +54,7 @@ export function MemberRequire({ badges, name }) {
             </>
           ) : (
             <>
-              <GroupJoinBtn onClick={handleJoinBtnToggle} />
+              <GroupJoinBtn text="Apply" onClick={handleJoinBtnToggle} />
             </>
           )}
 
